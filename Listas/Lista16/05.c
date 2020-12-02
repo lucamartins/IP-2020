ede48f055213e0067a958659b9ebfa8cdda51e3b
Luca S. Martins <65422994+lucamartins@users.noreply.github.com>
2020-12-02 15:59:06 -0300

    Fixed question 5

--- Listas/Lista16/05.c
+++ Listas/Lista16/05.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+// OBS: Cada linha do arquivo de entrada deve conter a cidade primeiramente e depois, separado por virgula, o numero de habitantes !
 
 int main(){
     char arq_entrada[60], arq_saida[60];
@@ -13,14 +16,20 @@ int main(){
     FILE *fp = fopen(arq_entrada, "r");
     FILE *fpOut = fopen(arq_saida, "w");
 
-    char aux_cidade[40];
-    int aux_num_hab = 0;
-
-    char cidade_mais_pop[40];
-    int num_hab = 0;
+    char aux_cidade[100], cidade_mais_pop[100];
+    int aux_num_hab = 0, num_hab = 0;
+ 
+    char buffer[1000], *piece;
 
     while(!feof(fp)){
-        fscanf(fp, "%s %d", aux_cidade, &aux_num_hab);
+        fgets(buffer, sizeof(buffer), fp);
+        buffer[strlen(buffer)-1]=0;
+
+        piece = strtok(buffer, ",");
+        strcpy(aux_cidade, piece);
+
+        piece = strtok(NULL, ",");
+        aux_num_hab = strtol(piece, NULL, 10);
 
         if(aux_num_hab > num_hab){
             strcpy(cidade_mais_pop, aux_cidade);
